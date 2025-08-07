59294cbfcc4b5f387d6d0e9da51bcb4c6e1f9f06
James Jiada Zhan <jzha832@emory.edu>
2025-08-07 16:39:46 -0400

    Annotate more EIC utilities

--- R/mass.match.R
+++ R/mass.match.R
@@ -12,10 +12,15 @@
 #'
 #' @keywords models
 mass.match <- function(x, known.mz, match.tol.ppm = 5) {
+    # 1. Prepare logical vector recording if each m/z has a match
     mass.matched.pos <- rep(0, length(x))
+    # 2. Loop over every input mass
     for (i in 1:length(x)) {
+        # 3. Compute fractional distance to each known m/z value
         this.d <- abs((x[i] - known.mz) / x[i])
+        # 4. Flag as matched when smallest distance is within ppm tolerance
         if (min(this.d) < match.tol.ppm / 1e6) mass.matched.pos[i] <- 1
     }
+    # 5. Return vector indicating which masses have matches
     return(mass.matched.pos)
 }
--- R/merge.seq.3.R
+++ R/merge.seq.3.R
@@ -12,16 +12,26 @@
 #'
 #' @keywords models
 merge.seq.3 <- function(a, mz, inte) {
+    # 1. Determine total number of time points
     l <- length(a)
+    # 2. Find boundaries where retention time changes
     breaks <- c(0, which(a[1:(l - 1)] != a[2:l]), l)
-    new.int <- new.mz <- rep(0, length(breaks) - 1) 
+    # 3. Prepare containers for merged intensities and m/z
+    new.int <- new.mz <- rep(0, length(breaks) - 1)
 
+    # 4. Iterate over contiguous time segments
     for (i in 1:(length(breaks) - 1)) {
+        # 5. Slice intensity values for this segment
         this.int <- inte[(breaks[i] + 1):breaks[i + 1]]
+        # 6. Slice m/z values for this segment
         this.mz <- mz[(breaks[i] + 1):breaks[i + 1]]
+        # 7. Sum intensities within the segment
         new.int[i] <- sum(this.int)
+        # 8. Record median m/z of peak with maximum intensity
         new.mz[i] <- median(this.mz[which(this.int == max(this.int))])
     }
+    # 9. Unique retention times for output
     new.a <- unique(a)
+    # 10. Return merged m/z, time, and intensity
     return(cbind(new.mz, new.a, new.int))
 }
